5589af1e655e9e94c3fd47259c6a03480ebc8ecc
zx-loh <59832510+Zx-Loh@users.noreply.github.com>
2024-07-05 12:41:42 +0800

    Finish substings project

--- substrings/substrings.rb
+++ substrings/substrings.rb
@@ -1,2 +1,18 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
+def substring(string, dictionary)
+    result = Hash.new(0)
+    letters = string.downcase.split(" ")
+
+    letters.map do |word|
+        dictionary.map do |substring|
+            if word.include?(substring)
+                result[substring] += 1
+            end
+        end
+    end
+    p result
+
+end
+
+substring("Howdy partner, sit down! How's it going?", dictionary)
