06d3add5e80b1c8b3e60cfb07048d76e4044524f
Zx Loh <59832510+Zx-Loh@users.noreply.github.com>
2024-08-30 16:55:13 +0800

    Add rubocop
    Installed rubocop into the caeser cipher folder to experiment around with linters and formatting

--- Caeser_Cipher/caeserCipher.rb
+++ Caeser_Cipher/caeserCipher.rb
@@ -1,35 +1,33 @@
-#ASCII Values for lowercase is 97 (a) to 122 (z)
-#Uppercase is 65 (A) to 90 (Z)
-#Space is 32
+# ASCII Values for lowercase is 97 (a) to 122 (z)
+# Uppercase is 65 (A) to 90 (Z)
+# Space is 32
 
 def caeserCipher(string, shift)
-    result = ""
-    characters = string.split("")
-    asciiValues = characters.map do |letter|
-                      letter.ord
-    end
+  result = ''
+  characters = string.split('')
+  asciiValues = characters.map do |letter|
+    letter.ord
+  end
 
-    shiftedValues = asciiValues.map do |integer|
-        if integer == 32          
-            integer
-        else 
-            integer + shift
-        end
+  shiftedValues = asciiValues.map do |integer|
+    if integer == 32
+      integer
+    else
+      integer + shift
     end
+  end
 
-    shiftedValues.map do |integer|
-        if integer > 122 || (integer > 90 && integer < 97)
-            integer -= 26
-        end
-        result += integer.chr
-    end
+  shiftedValues.map do |integer|
+    integer -= 26 if integer > 122 || (integer > 90 && integer < 97)
+    result += integer.chr
+  end
 
-    puts "Encoded Text:"
-    puts result
+  puts 'Encoded Text:'
+  puts result
 end
 
-puts "Please key in the string you would like to shift:"
+puts 'Please key in the string you would like to shift:'
 string = gets.chomp
-puts "How much would you like to shift it by?"
+puts 'How much would you like to shift it by?'
 shift = gets.chomp.to_i
 caeserCipher(string, shift)
